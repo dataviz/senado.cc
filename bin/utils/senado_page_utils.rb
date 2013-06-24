31540da1a9ebdc0ceaddcf0dfa784c7323b77d37
Thiago Bueno <tbueno@tbueno.com>
2013-06-24 23:10:10 +0200

    Image downloader script

---
+++ bin/utils/senado_page_utils.rb
@@ -0,0 +1,33 @@
+module Senado
+  BASE_URL = 'http://www.senado.gov.br/senadores/'
+  SENADOR_URL = 'dinamico/paginst/senador'
+
+  SENADORES = {"4981" => "Acir Gurgacz (PDT-RO)", "391" => "Aécio Neves (PSDB-MG)", "4527" => "Alfredo Nascimento (PR-AM)",
+             "846" => "Aloysio Nunes Ferreira (PSDB-SP)", "945" => "Alvaro Dias (PSDB-PR)", "4988" => "Ana Amélia (PP-RS)",
+             "4869" => "Ana Rita (PT-ES)", "4697" => "Angela Portela (PT-RR)", "4837" => "Anibal Diniz (PT-AC)",
+             "5150" => "Antonio Carlos Rodrigues (PR-SP)", "3" => "Antonio Carlos Valadares (PSB-SE)",
+             "715" => "Armando Monteiro (PTB-PE)", "5164" => "Ataídes Oliveira (PSDB-TO)", "3823" => "Benedito de Lira (PP-AL)",
+             "111" => "Blairo Maggi (PR-MT)", "13" => "Casildo Maldaner (PMDB-SC)", "5197" => "Cássio Cunha Lima (PSDB-PB)",
+             "4529" => "Cícero Lucena (PSDB-PB)", "739" => "Ciro Nogueira (PP-PI)", "4895" => "Clésio Andrade (PMDB-MG)",
+             "3398" => "Cristovam Buarque (PDT-DF)", "4877" => "Cyro Miranda (PSDB-GO)", "3360" => "Delcídio do Amaral (PT-MS)",
+             "4721" => "Eduardo Amorim (PSC-SE)", "4994" => "Eduardo Braga (PMDB-AM)", "4767" => "Eduardo Lopes (PRB-RJ)",
+             "17" => "Eduardo Suplicy (PT-SP)", "20" => "Epitácio Cafeteira (PTB-MA)", "612" => "Eunício Oliveira (PMDB-CE)",
+             "4525" => "Fernando Collor (PTB-AL)", "3634" => "Flexa Ribeiro (PSDB-PA)", "765" => "Francisco Dornelles (PP-RJ)",
+             "4935" => "Garibaldi Alves (PMDB-RN)", "4776" => "Gim (PTB-DF)", "5008" => "Humberto Costa (PT-PE)",
+             "613" => "Inácio Arruda (PC DO B-CE)", "5004" => "Ivo Cassol (PP-RO)", "35" => "Jader Barbalho (PMDB-PA)",
+             "4545" => "Jarbas Vasconcelos (PMDB-PE)", "4531" => "Jayme Campos (DEM-MT)", "950" => "João Alberto Souza (PMDB-MA)",
+             "3394" => "João Capiberibe (PSB-AP)", "4537" => "João Durval (PDT-BA)", "4541" => "João Vicente Claudino (PTB-PI)",
+             "4990" => "Jorge Viana (PT-AC)", "40" => "José Agripino (DEM-RN)", "615" => "José Pimentel (PT-CE)",
+             "47" => "José Sarney (PMDB-AP)", "1249" => "Kátia Abreu (PSD-TO)", "4575" => "Lídice da Mata (PSB-BA)",
+             "3695" => "Lindbergh Farias (PT-RJ)", "3579" => "Lobão Filho (PMDB-MA)", "643" => "Lúcia Vânia (PSDB-GO)",
+             "5002" => "Luiz Henrique (PMDB-SC)", "631" => "Magno Malta (PR-ES)", "1023" => "Maria do Carmo Alves (DEM-SE)",
+             "4539" => "Mário Couto (PSDB-PA)", "952" => "Mozarildo Cavalcanti (PTB-RR)", "3741" => "Paulo Bauer (PSDB-SC)",
+             "5020" => "Paulo Davim (PV-RN)", "825" => "Paulo Paim (PT-RS)", "68" => "Pedro Simon (PMDB-RS)",
+             "5010" => "Pedro Taques (PDT-MT)", "5012" => "Randolfe Rodrigues (PSOL-AP)", "70" => "Renan Calheiros (PMDB-AL)",
+             "635" => "Ricardo Ferraço (PMDB-ES)", "72" => "Roberto Requião (PMDB-PR)", "4593" => "Rodrigo Rollemberg (PSB-DF)",
+             "73" => "Romero Jucá (PMDB-RR)", "4893" => "Ruben Figueiró (PSDB-MS)", "4560" => "Sérgio Petecão (PSD-AC)",
+             "5048" => "Sérgio Souza (PMDB-PR)", "3372" => "Valdir Raupp (PMDB-RO)", "558" => "Vanessa Grazziotin (PC DO B-AM)",
+             "4763" => "Vicentinho Alves (PR-TO)", "4645" => "Vital do Rêgo (PMDB-PB)", "1176" => "Waldemir Moka (PMDB-MS)",
+             "604" => "Walter Pinheiro (PT-BA)", "5016" => "Wellington Dias (PT-PI)", "5070" => "Wilder Morais (DEM-GO)",
+             "5144" => "Zeze Perrella (PDT-MG)"}
+end
